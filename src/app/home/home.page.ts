195763f7a5e2f7b61b2cbf6d5799c246cb6d1d53
Boston <g00436544@atu.ie>
2026-03-27 13:28:36 +0000

    Done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { MovieServices } from '../Services/movie-services';
+import { IonButton,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton,IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
 })
 export class HomePage {
-  constructor() {}
+  movies:any[]=[];
+  constructor(private service:MovieServices) {}
+
+  ionViewWillEnter(){
+    this.service.GetMovieData().subscribe(
+      (data)=>{
+        this.movies = data.Search;
+        console.log(this.movies);
+      }
+    );
+  }
 }
