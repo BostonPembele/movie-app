951e7fa6923ae5806e8ec27299a53182f1c24381
Boston <g00436544@atu.ie>
2026-03-27 13:38:47 +0000

    fINALLY ADDED COMMENTS I AM FINISHED

--- src/app/Services/movie-services.ts
+++ src/app/Services/movie-services.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+// imported httpClient and observable to the Services.ts
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -8,6 +9,7 @@ import { Observable } from 'rxjs';
 export class MovieServices {
   constructor(private httpClient:HttpClient){}
 
+  //Used the api from the moodle page to be able to access the contents of the array
   GetMovieData():Observable<any>{
     return this.httpClient.get('https://www.omdbapi.com/?apikey=2ee2ed84&s=%27war%27')
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+//imported MovieServices from the services folder to access the items of the services
 import { MovieServices } from '../Services/movie-services';
+//added card components for the ionic ui website to help display the project using cards
 import { IonButton,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  //the imports of the import above
   imports: [IonButton,IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
 })
 export class HomePage {
   movies:any[]=[];
+  //added an object service to the constructor
   constructor(private service:MovieServices) {}
 
+  //Fuction to access the api content 
   ionViewWillEnter(){
     this.service.GetMovieData().subscribe(
       (data)=>{
